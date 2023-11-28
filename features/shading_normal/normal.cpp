66d859e04597e74fc6a848cdf102f0326fee8b14
Zhongzhu Deng <dzhongzhu@icloud.com>
2023-11-28 09:00:37 +0100

    fix(shading normal): use bilinear interplotation

--- features/shading_normal/normal.cpp
+++ features/shading_normal/normal.cpp
@@ -8,8 +8,21 @@ class NormalTexture : public Texture {
         Repeat,
     };
 
+    enum class FilterMode {
+        Nearest,
+        Bilinear,
+    };
+
     ref<Image> m_image;
     BorderMode m_border;
+    FilterMode m_filter;
+
+    static inline Color lerp(Color v0, Color v1, float x) {
+        Color res;
+        for (int i = 0; i < 3; i++)
+            res[i] = std::lerp(v0[i], v1[i], x);
+        return res;
+    }
 
 public:
     NormalTexture(const Properties &properties) {
@@ -26,32 +39,84 @@ public:
                                                {"clamp", BorderMode::Clamp},
                                                {"repeat", BorderMode::Repeat},
                                            });
+
+        m_filter = properties.getEnum<FilterMode>(
+            "filter",
+            FilterMode::Bilinear,
+            {
+                {"nearest", FilterMode::Nearest},
+                {"bilinear", FilterMode::Bilinear},
+            });
     }
 
     Color evaluate(const Point2 &uv) const override {
         Point2i resolution = m_image->resolution();
-        Point2i pixel;
+        Point2 m_uv;
+        Point2 m_point;
+        Point2i pixels[4];
+        Color colors[4], u0, u1;
+        double s, t;
+        m_uv.x() = uv.x() - floor(uv.x());
+        m_uv.y() = 1 - (uv.y() - floor(uv.y()));
 
         if (m_border == BorderMode::Clamp) {
-            pixel.x() = std::clamp(
+            m_point.x() = std::clamp(
                 (int)(uv.x() * resolution.x()), 0, resolution.x() - 1);
-            pixel.y() = std::clamp(
+            m_point.y() = std::clamp(
                 (int)((1 - uv.y()) * resolution.y()), 0, resolution.y() - 1);
         } else {
-            pixel.x() =
-                std::min((int)((uv.x() - floor(uv.x())) * resolution.x()),
-                         resolution.x() - 1);
-            pixel.y() =
-                std::min((int)((1 - (uv.y() - floor(uv.y()))) * resolution.y()),
-                         resolution.y() - 1);
+            m_point.x() = m_uv.x() * resolution.x();
+            m_point.y() = m_uv.y() * resolution.y();
         }
-        return (*m_image)(pixel);
+
+        if (m_filter == FilterMode::Nearest) {
+            pixels[3].x() = std::min((int)m_point.x(), resolution.x() - 1);
+            pixels[3].y() = std::min((int)m_point.y(), resolution.y() - 1);
+            return (*m_image)(pixels[3]);
+        }
+
+        if (m_border == BorderMode::Clamp) {
+            m_point.x() = uv.x() * resolution.x();
+            m_point.y() = (1 - uv.y()) * resolution.y();
+        }
+
+        pixels[3].x() = round(m_point.x());
+        pixels[3].y() = round(m_point.y());
+        pixels[0].x() = pixels[3].x() - 1;
+        pixels[0].y() = pixels[3].y() - 1;
+        pixels[1].x() = pixels[3].x() - 1;
+        pixels[1].y() = pixels[3].y();
+        pixels[2].x() = pixels[3].x();
+        pixels[2].y() = pixels[3].y() - 1;
+
+        for (int i = 0; i < 4; i++) {
+            if (m_border == BorderMode::Clamp) {
+                pixels[i].x() =
+                    std::clamp(pixels[i].x(), 0, resolution.x() - 1);
+                pixels[i].y() =
+                    std::clamp(pixels[i].y(), 0, resolution.y() - 1);
+            } else {
+                pixels[i].x() =
+                    (pixels[i].x() + resolution.x()) % resolution.x();
+                pixels[i].y() =
+                    (pixels[i].y() + resolution.y()) % resolution.y();
+            }
+            colors[i] = (*m_image)(pixels[i]);
+        }
+
+        s = (m_uv.x() * resolution.x() - round(m_uv.x() * resolution.x())) +
+            0.5;
+        t = (m_uv.y() * resolution.y() - round(m_uv.y() * resolution.y())) +
+            0.5;
+        u0 = lerp(colors[0], colors[2], s);
+        u1 = lerp(colors[1], colors[3], s);
+        return lerp(u0, u1, t);
     }
 
     std::string toString() const override {
         return tfm::format(
             "NormalTexture[\n"
-            "  image = %s,\n"
+            "  image = %s\n"
             "]",
             indent(m_image));
     }
